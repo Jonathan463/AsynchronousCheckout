bf46b13d2dc60180d72db617c4e61a007fdd5190
Jonathan Chidubem Godson <godsonjonathan39@gmail.com>
2026-04-05 21:21:48 +0100

    implemented createProduct

--- src/main/java/technologyforall/com/asynchronouscheckout/service/serviceImpl/ProductServiceImpl.java
+++ src/main/java/technologyforall/com/asynchronouscheckout/service/serviceImpl/ProductServiceImpl.java
@@ -1,12 +1,31 @@
 package technologyforall.com.asynchronouscheckout.service.serviceImpl;
 
+import lombok.RequiredArgsConstructor;
+import technologyforall.com.asynchronouscheckout.model.Product;
 import technologyforall.com.asynchronouscheckout.model.dto.ProductRequest;
 import technologyforall.com.asynchronouscheckout.model.dto.ProductResponse;
+import technologyforall.com.asynchronouscheckout.repository.ProductRepository;
 import technologyforall.com.asynchronouscheckout.service.ProductService;
 
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+
+    ProductRepository productRepository;
+
     @Override
     public ProductResponse createProduct(ProductRequest productRequest) {
-        return null;
+
+        Product product = new Product();
+        product.setProductName(product.getProductName());
+        product.setProductSize(productRequest.getProductSize());
+        product.setProductSize(product.getProductSize());
+
+        Product savedProduct = productRepository.save(product);
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setProductName(savedProduct.getProductName());
+        productResponse.setProductSize(savedProduct.getProductSize());
+        productResponse.setProductSize(savedProduct.getProductSize());
+        return productResponse;
     }
 }
